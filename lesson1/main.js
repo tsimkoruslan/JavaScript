44b066ff70671a34be3005c95b6bb24e6551cdd7
tsimkoruslan <tsimkoruslan@gmail.com>
2023-02-01 16:19:02 +0200

    Initial commit

---
+++ lesson1/main.js
@@ -0,0 +1,39 @@
+let a = `hello` ;
+let b = `owu` ;
+let c = `com` ;
+let d = `ua` ;
+
+let namberA = 1 ;
+let namberB = 10 ;
+let namberC = -999 ;
+let namberD = 123 ;
+const PI = 3.14 ;
+let namberE = 2.7 ;
+let namberF = 16 ;
+let boolT = true ;
+let boolF = false ;
+
+console.log(`${a} ${b} ${c} ${d} ${namberA} ${namberB} ${namberC} ${namberD} ${namberE} ${namberF} ${boolT} ${boolF} ${PI}`) ;
+
+let firstName = `Tsimko ` ;
+let middleName = `Ruslan ` ;
+let lastName = `Igorovich ` ;
+
+let person = firstName + middleName + lastName
+console.log(person);
+
+let a1 = 100;
+let b2 = '100';
+let c3 = true;
+
+console.log(typeof(a1)) ;
+console.log(typeof(b2)) ;
+console.log(typeof(c3)) ;
+
+let name = prompt(`Як вас звати ?`);
+let phatherName = prompt(`Як ви по батькові ?`);
+let years = prompt (`Скільки вам років ?`);
+
+let person1 = `${name} ${phatherName} ${years}`
+
+console.log(person1);
