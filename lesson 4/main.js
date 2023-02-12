9dbc25740417d040eecf82cccf60a79eddaa0986
tsimkoruslan <tsimkoruslan@gmail.com>
2023-02-12 19:36:05 +0200

    additional 4

--- lesson 4/main.js
+++ lesson 4/main.js
@@ -135,8 +135,15 @@ console.log(sumNumber(sumArray))
 // - створити функцію swap(arr,index1,index2). Функція міняє місцями заняення у відаовідних індексах
 
 // Приклад  swap([11,22,33,44],0,1) //=> [22,11,33,44]
+let qwert = [11,22,33,44];
 function swap (arr , index1 , index2 ){
-
+    let container = arr[index1];
+        arr[index1] = arr[index2];
+        arr[index2] = container;
+                    // arr.splice(index1, 1, arr.splice(index2, 1, arr[index1])[0]);
+    return arr;
 }
+console.log(swap(qwert,2,1))
+
 // - Написати функцію обміну валюти exchange(sumUAH,currencyValues,exchangeCurrency)
 // Приклад exchange(10000,[{currency:'USD',value:40},{currency:'EUR',value:42}],'USD') // => 250
