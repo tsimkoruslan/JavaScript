abf149234341ca20e850380ae0eb0bd1683baa1e
tsimkoruslan <tsimkoruslan@gmail.com>
2023-03-11 23:21:45 +0200

    mini_project

--- mini_project/post/main.js
+++ mini_project/post/main.js
@@ -3,30 +3,46 @@ let url = new URL(location.href);
 let post = JSON.parse(url.searchParams.get('post'));
 //create DOM
 let parentDiv = document.createElement('div');
+parentDiv.classList.add('ParentDiv')
 let title = document.createElement('h1');
+title.classList.add('title');
 let idInfoP = document.createElement('p');
+idInfoP.classList.add('idInfoP');
 let bodyPost = document.createElement('div');
+bodyPost.classList.add('bodyPost');
+
         title.innerText = `"${post.title}"`;
         idInfoP.innerText = `ID - ${post.id} User ID - ${post.userId}`;
-        bodyPost.innerText = post.body;
+        bodyPost.innerText = `⬊ ${post.body} ⬉`;
         parentDiv.append(title,idInfoP,bodyPost);
             document.body.append(parentDiv);
 //create comments of post
+let divComment  = document.createElement('div');
+divComment.classList.add('divComment');
+let textComment  = document.createElement('h3');
+textComment.innerText = '↓ comments ↓';
+document.body.append(textComment);
+textComment.classList.add('textComment');
 fetch(` https://jsonplaceholder.typicode.com/posts/${post.id}/comments`)
     .then(value => value.json())
     .then(comments => {
         for (let comment of comments) {
             let div = document.createElement('div');
             let nameCommentH4 = document.createElement('h4');
+            nameCommentH4.classList.add('nameCommentH4');
             let bodyCommentP = document.createElement('p');
             let email = document.createElement('p');
+            email.classList.add('email');
                     nameCommentH4.innerText = `${comment.name}`;
                     bodyCommentP.innerText = `"${comment.body}"`;
                     email.innerText = `${comment.email}`;
             div.append(nameCommentH4,bodyCommentP,email);
-            document.body.append(div);
+            div.classList.add('childComment');
+            divComment.append(div)
+            document.body.append(divComment);
         }
 
     })
+
 
 
--- mini_project/user/main.js
+++ mini_project/user/main.js
@@ -2,7 +2,7 @@
 let url = new URL(location.href);
 let user = JSON.parse(url.searchParams.get(`user`));
 let parentDiv = document.createElement('div');
-
+parentDiv.classList.add('parentDiv');
 //recursive output of all items
 function recurs (item){
     for (let [key, value] of Object.entries(item)) {
@@ -24,24 +24,33 @@ recurs(user)
 
 //creat <button> (post of current user) and (post_details)
 let button = document.createElement('button');
-let titleDiv = document.createElement('div')
+let titleDiv = document.createElement('div');
+titleDiv.classList.add('titleDiv');
+titleDiv.id = 'box';
 button.innerText = 'post of current user';
     button.onclick = function (ev){
         ev.preventDefault();
         fetch(`https://jsonplaceholder.typicode.com/users/${user.id}/posts`)
             .then(value => value.json())
             .then(posts => {
+                button.disabled = true;
+                button.innerText = '↓ Post user ↓'
                 for (const post of posts) {
+                    let parent = document.createElement('div');
                    let title =  document.createElement('h4');
+                   title.classList.add('titleComment');
                    let button = document.createElement('button');
-                   let a = document.createElement('a');
-                             title.innerText = `"${post.title}"`;
-                             titleDiv.appendChild(title);
-                    a.innerText = 'post-details';
-                    a.href = '../post/post-details.html?post=' + JSON.stringify(post);
-                    button.appendChild(a);
-                         titleDiv.appendChild(button);
+                   title.innerText = `"${post.title}"`;
+                    parent.appendChild(title);
+                    parent.classList.add('parentComment');
+                    button.innerText = 'post-details ⤿';
+                    button.onclick = function() {
+                        window.location.href = '../post/post-details.html?post=' + JSON.stringify(post);
+                    };
+                    parent.appendChild(button);
+                    titleDiv.appendChild(parent)
                               document.body.appendChild(titleDiv);
+                    titleDiv.scrollIntoView({block: "center", behavior: "smooth"});
                 }
             })
     }
